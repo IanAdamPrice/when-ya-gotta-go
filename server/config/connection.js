ca37e02334e77c5563882279dab33317e8d1924b
IanAdamPrice <ianadamprice@gmail.com>
2022-04-07 21:20:35 -0400

    heroku testing'

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/whenyagottago', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/when-ya-gotta-go', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql `
     _id: ID!
     username: String
     email: String
-    roomCount: Int
+    bathroomCount: Int
     savedRooms: [Bathroom]
   }
 
