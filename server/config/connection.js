136b1e9632a77afb665d57b3ac1819acbcd6b048
IanAdamPrice <ianadamprice@gmail.com>
2022-04-07 22:37:35 -0400

    heroku testing'

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/when-ya-gotta-go', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/whenyagottago', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
